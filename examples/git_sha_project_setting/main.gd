73bae0ff2e1f43198a7363c372173ed463fe5baa
Chetter <chettercsgo@mail.ru>
2025-03-15 21:07:56 +0700

    Test2

--- examples/git_sha_project_setting/main.gd
+++ examples/git_sha_project_setting/main.gd
@@ -5,8 +5,8 @@ extends PanelContainer
 @export var _version_complete: Label
 
 func _ready() -> void:
-    var version: String = ProjectSettings.get_setting("application/config/version", "<not set>")
-    var git_sha: String = ProjectSettings.get_setting("application/config/git_sha", "<unavailable>")
-    self._version.text = version
-    self._git_sha.text = git_sha
-    self._version_complete.text = version + "-" + git_sha.substr(0, 6)
+	var version: String = ProjectSettings.get_setting("application/config/version", "<not set>")
+	var git_sha: String = ProjectSettings.get_setting("application/config/git_sha", "<unavailable>")
+	self._version.text = version
+	self._git_sha.text = git_sha
+	self._version_complete.text = version + "-" + git_sha.substr(0, 6)
